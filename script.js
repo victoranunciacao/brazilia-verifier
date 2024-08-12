c39fcc237b73f54cfb6a5f1baba17a884172e35c
victoranunciacao <vikktormiguell@gmail.com>
2024-08-12 00:42:45 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+function verificar() {
+    var valor = document.getElementById('valor');
+    var result = (valor.value)
+
+        if (result == 'Brasil') {
+            alert('Olá, Brasileiro!')
+        } else {
+            alert('Olá, Estrangeiro!')
+        }
+}
